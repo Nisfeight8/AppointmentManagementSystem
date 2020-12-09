c1b05bf05d91f176b4e24538a6c4ab84f0bcdc87
Nisfeight <fanispapazoglou1999@gmail.com>
2020-12-09 18:35:12 +0200

    return employees carrier implemented

--- src/main/java/appointments/management/system/controllers/EmployeeController.java
+++ src/main/java/appointments/management/system/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package appointments.management.system.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import appointments.management.system.entities.Appointment;
+import appointments.management.system.entities.Carrier;
+import appointments.management.system.exception.CustomException;
 import appointments.management.system.services.AppointmentService;
 import appointments.management.system.services.CarrierService;
+import appointments.management.system.services.EmployeeService;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -25,8 +29,18 @@ public class EmployeeController {
 	@Autowired
 	CarrierService carrierService;
 	@Autowired
+	EmployeeService employeeService;
+	@Autowired
 	AppointmentService appService;
-	
+	@GetMapping("/{id}/carriers")
+	@ResponseBody
+	public Carrier GetCarrier(@PathVariable("id") int id) {
+		if (employeeService.findById(id).getCarrier() == null) {
+			throw new CustomException("No carrier found", HttpStatus.UNPROCESSABLE_ENTITY);
+		}else {
+		return employeeService.findById(id).getCarrier();
+		}
+	}
 	@GetMapping("/carriers/{id}/appointments")
 	@ResponseBody
 	public Collection<Appointment> AppointmentsByCarrier(@PathVariable("id") int id) {
